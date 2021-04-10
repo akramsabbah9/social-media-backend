bc63deb492499e6b3577561c2dbd44a1154cc351
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-10 16:04:45 -0700

    added User model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+/* models/User.js: define User schema */
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: "A username must be provided.",
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: "An email must be provided.",
+            matches: [
+                /([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/,
+                "This is not a valid email address."
+            ]
+        },
+        thoughts, // TODO: [{type: Schema.Types.ObjectId, ref: "Thought"}]
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: "User" // self-reference
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+const User = model("User", UserSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+/* models/index.js: package models in an object and export to controllers */
+const User = require("./User");
+
+module.exports = { User };
