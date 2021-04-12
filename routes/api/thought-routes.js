d8fdccf09febb85f83ab72e8bd6658522dc2f95e
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-11 21:30:01 -0700

    added ability to add and remove reactions in thoughts

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,6 +1,7 @@
 /* routes/api/thought-routes.js: express routes for /api/thoughts endpoint */
 const router = require("express").Router();
 const thoughtController = require("../../controllers/thought-controller");
+const reactionController = require("../../controllers/reaction-controller");
 
 // routes for /api/thoughts/
 router.route("/")
@@ -12,5 +13,11 @@ router.route("/:id")
     .get(thoughtController.getOneThought)
     .put(thoughtController.updateThought)
     .delete(thoughtController.deleteThought);
+
+// route for api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(reactionController.addReaction);
+
+// route for api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(reactionController.removeReaction);
 
 module.exports = router;
